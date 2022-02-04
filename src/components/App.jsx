b48318f24560d63e31505aa7c89d8fd194937b7c
alexaMRR <44932717+alexaMRR@users.noreply.github.com>
2022-02-03 18:08:26 -0800

    Making a trivia game

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,16 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  return <div className="app">Trivia!</div>;
+  return ( <div className="app"> Trivia! 
+  <Question text = "Questions goes here" />
+
+  </div>
+  
+)
+} 
+function Question(props) {
+  let text = props.text
+
 }
 
 export default App;
