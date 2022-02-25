472e9de84be1301876e47d246872e9e7d5dfbddb
alexaMRR <44932717+alexaMRR@users.noreply.github.com>
2022-02-24 18:06:09 -0800

    Added map functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,45 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  return ( <div className="app"> Trivia! 
-  <Question text = "Questions goes here" />
+  let question = 0;
+  const [answerDisplay, setAnswerDisplay] = useState(false);
+  return (
+    <div className="app">
+      {" "}
+      Trivia!
+      <Question
+        text={data[question].question.text}
+        choices={data[question].question.choices}
+      />
+      <button>{useState(true)}</button>
 
-  </div>
-  
-)
-} 
-function Question(props) {
-  let text = props.text
+      <NextQuestion />
+
+    </div>
+  );
+}
+function answerHandle {
 
 }
-
+function Question(props) {
+  let text = props.text;
+  const choices = props.choices;
+  const answers = choices.map((choice) =>
+  <Answer text={choice}/>)
+  
+  return (
+    <div className="words">
+      {" "}
+      {props.text}
+      {answers}
+    </div>
+  );
+}
+function NextQuestion(props) {
+  return <button> Next Question </button>;
+}
+function Answer(props) {
+  let answer = props.text;
+  return <div className="words"> {props.text} </div>;
+}
 export default App;
